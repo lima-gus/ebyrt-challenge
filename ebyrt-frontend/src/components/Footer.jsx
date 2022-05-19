6e38f544a078fdd002071a73c041aa0f5f9f6406
Gustavo Lima <lima_gus@outlook.com>
2022-05-19 15:52:11 -0300

    feat (frontend): Footer component

---
+++ ebyrt-frontend/src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../styles/footer.css';
+
+function Footer() {
+  return (
+    <footer>
+      <div className="footer-container">
+        <p>Made with 💚 by <a 
+          href="https://www.linkedin.com/in/lima-gus/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          Gustavo
+          </a>
+        </p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
