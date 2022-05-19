6b85cc97ac7c7a6973862bd8283b97482c292006
Gustavo Lima <lima_gus@outlook.com>
2022-05-19 18:20:02 -0300

    feat (frontend): Tasks component - hard coded

--- ebyrt-frontend/src/App.js
+++ ebyrt-frontend/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Tasks from './components/Tasks';
 
 function App() {
   return (
     <div>
       <Header />
+      <Tasks />
       <Footer />
     </div>
   );
